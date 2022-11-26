cb9663bf5af4ccb71680144a018546a5c3ca3e5c
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2022-11-25 18:38:15 -0800

    Delete Kata-Training: Square Every Digit.py

--- Kata-Training: Square Every Digit.py
+++
@@ -1,5 +0,0 @@
-def square_digits(num):
-    numbers = ''.join(str(int(number)**2) for number in str(num)) #seperate each numbers and squre them
-    return int(numbers) # return the numbers
-    
-square_digits() # any numbers you can put
