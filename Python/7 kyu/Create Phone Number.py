f9867722ed96a6319556725e669a19161cb41ea6
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2022-11-29 23:00:53 -0800

    Delete Create Phone Number.py

--- Python/7 kyu/Create Phone Number.py
+++
@@ -1,8 +0,0 @@
-def create_phone_number(n):
-    number = n
-    digit = ''.join([str(num) for num in number])
-    
-    return f'({digit[:3]}) {digit[3:6]}-{digit[6:10]}'
-  
-  
-  ###https://github.com/hevalhazalkurt/codewars_python_solutions/blob/master/6kyuKatas/Create_Phone_Number.md
