390a5d69fbd6fa976222a141a47ed72071f258fc
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2022-12-04 21:56:56 -0800

    Create Find the stray number.py

---
+++ Python/7 kyu/Find the stray number.py
@@ -0,0 +1,12 @@
+def stray(arr):
+    leastCount, leastElement = len(arr), -99
+    for i in range(len(arr)):
+        currentCount = 0
+        for j in range(len(arr)):
+            if (arr[i] == arr[j]):
+                currentCount += 1
+        if (currentCount < leastCount):
+            leastCount, leastElement = currentCount, arr[i]
+    return leastElement
+  
+  ###https://iq.opengenus.org/least-frequent-element-in-array/
