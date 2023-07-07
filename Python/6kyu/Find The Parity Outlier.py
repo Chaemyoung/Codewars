bfabb15c8a60e38b382896f5655d6c5f838f94a4
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-07-06 21:58:31 -0700

    Update Find The Parity Outlier.py

--- Python/6kyu/Find The Parity Outlier.py
+++ Python/6kyu/Find The Parity Outlier.py
@@ -1,19 +1,4 @@
-def find_outlier(arr):
-    even_count = 0
-    odd_count = 0
-    last_even = None
-    last_odd = None
-
-    for num in arr:
-        if num % 2 == 0:
-            even_count += 1
-            last_even = num
-        else:
-            odd_count += 1
-            last_odd = num
-
-        # Check if we have identified both even and odd elements
-        if even_count > 0 and odd_count > 0:
-            break
-
-    return last_even if even_count == 1 else last_odd
+def find_outlier(integers):
+    odds = [x for x in integers if x%2!=0]
+    evens= [x for x in integers if x%2==0]
+    return odds[0] if len(odds)<len(evens) else evens[0]
