738b5cd65b915cf6b7df261d59fbac8f5aff2c85
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-09-24 12:32:33 -0700

    Update Exes and Ohs.py

--- Python/7 kyu/Exes and Ohs.py
+++ Python/7 kyu/Exes and Ohs.py
@@ -1,15 +1,19 @@
 def xo(s):
+    # Record number of x and o
     count_x = 0
     count_o = 0
-    
+
+    # Make the sentence lowercase
     lower_s = s.lower()
-    
+
+    # If you find letter x, add 1 to count_x, if find o, add 1 to count_o
     for letter in lower_s:
         if letter == "x":
             count_x += 1
         elif letter == "o":
             count_o += 1
-    
+
+    # If the counts are equal, return True, If they are diff, return False
     if count_x == count_o:
         return True
     else:
